b89de1031f07bbdd67588e6c360c7a5913b62f42
mindziukasss <mindziukasss@gmail.com>
2017-01-25 14:12:06 +0200

    Added posts model and migrated to db

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Posts(models.Model):
+			title = models.CharField(max_length=255)
+			pub_date = models.DateTimeField()
+			image = models.ImageField(upload_to='media/')
+			body = models.TextField()
